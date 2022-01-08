3414f0ec9f3e36ef455b7deecbdc14105737eaa7
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-01-07 22:14:41 -0500

    added exercise 6-7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,62 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "Barbra",
+  last_name: "Gordon",
+  hourly_rate: 60
+)
+
+@store1.employees.create(
+  first_name: "Bruce",
+  last_name: "Wayne",
+  hourly_rate: 100
+)
+
+@store1.employees.create(
+  first_name: "Dinah",
+  last_name: "Laurel Lance",
+  hourly_rate: 80
+)
+
+@store1.employees.create(
+  first_name: "Selina",
+  last_name: "Kyle",
+  hourly_rate: 60
+)
+
+@store1.employees.create(
+  first_name: "Helena",
+  last_name: "Bertinelli",
+  hourly_rate: 60
+)
+
+@store2.employees.create(
+  first_name: "Helena",
+  last_name: "Bertinelli",
+  hourly_rate: 60
+)
+
+@store2.employees.create(
+  first_name: "Natasha",
+  last_name: "Romanova",
+  hourly_rate: 60
+)
+
+@store2.employees.create(
+  first_name: "Bucky",
+  last_name: "Barnes",
+  hourly_rate: 60
+)
+
+@store2.employees.create(
+  first_name: "Carol",
+  last_name: "Danvers",
+  hourly_rate: 60
+)
+
+@store2.employees.create(
+  first_name: "Clint",
+  last_name: "Barton",
+  hourly_rate: 60
+)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ApplicationRecord
+  validates :first_name, presence: true, 
+  validates :last_name, presence: true,
+
+  validates :hourly_rate, :inclusion => 40..200
+  
+end
+
+class Store < ApplicationRecord
+validates :name, length: {minimum: 3, maximum: 50}
+validates :annual_revenue, numericality: { only_integer: true }
+
+end
+
+# class Apparel_Required < ApplicationRecord
+# validate :mens_or_womens_required
+
+# def mens_or_womens_required
+#   if 
+# end
+
+puts "Please provide store name:"
+@store_name = get.chomp 
+
+new_store = Store.create(name: store_name)
+puts new_store.valid?
+puts new_store.errors.full_messages
