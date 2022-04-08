3fa8a51d7fc021ee557ac65359ab64762d445513
TJ-Blinn <tjblinn@hotmail.com>
2022-04-08 19:36:58 +0000

    completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,22 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# One-to-many association
+
+# Add the following code directly inside the Store model (class): has_many :employees
+# Add the following code directly inside the Employee model (class): belongs_to :store
+
+# Add some data into employees. (See exercvise #2)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Rubble", last_name: "Paws", hourly_rate: 60)
+@store1.employees.create(first_name: "Chase", last_name: "Paws", hourly_rate: 60)
+@store1.employees.create(first_name: "Rocky", last_name: "Paws", hourly_rate: 60)
+@store1.employees.create(first_name: "Skye", last_name: "Paws", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Lennon", hourly_rate: 60)
+@store2.employees.create(first_name: "Paul", last_name: "McCartney", hourly_rate: 60)
+@store2.employees.create(first_name: "Ringo", last_name: "Starr", hourly_rate: 60)
+@store2.employees.create(first_name: "George", last_name: "Harrison", hourly_rate: 60)
+
+
