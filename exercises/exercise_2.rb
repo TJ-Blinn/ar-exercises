f6bad3f2f0d20b4eafee05bbef6cb574b8833bc8
TJ-Blinn <tjblinn@hotmail.com>
2022-04-08 15:43:53 +0000

    completed exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: "Montreal")
+puts "The name of Store 1 has changed to #{@store1.name}"
