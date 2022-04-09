65a49af654e764b374a6a5b223ab5ba21e5ade14
TJ-Blinn <tjblinn@hotmail.com>
2022-04-09 00:51:48 +0000

    completed all except for displaying validation errors

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,14 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Validations for both models
+Store.create(name: "Name1", annual_revenue: 1000000, mens_apparel: false, womens_apparel: false )
+
+puts "Please provide store name:"
+@store_name = gets.chomp.to_s  
+
+new_store = Store.create(name: @store_name)
+# puts new_store.valid?
+# puts new_store.errors.full_messages
+
+p "Is the store name valid? #{new_store.errors.on(:name)} "
